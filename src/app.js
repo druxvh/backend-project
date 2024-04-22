f0edd9cb5c5fea9d297125bec4fdc90fb0134910
dm <88588404+druxvh@users.noreply.github.com>
2024-04-22 19:13:09 +0000

    add asyncHandler and apierrors and apiresponse

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+const app = express()
+
+
+// 'use' method is used to handle the middlewares 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+app.use(express.static("public"))
+
+// helps perform CRUD on user's saved cookies
+app.use(cookieParser())
+
+export default app
